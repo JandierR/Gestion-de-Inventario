be20723b4a732ab312b3e0ce51204dc6074545d4
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-25 12:13:10 -0600

    Clase Vertice creada

---
+++ src/Vertice.java
@@ -0,0 +1,19 @@
+public class Vertice {
+    private final String nombre;
+    private final int distancia;
+
+
+    public Vertice(String nombre, int distancia) {
+        this.nombre = nombre;
+        this.distancia = distancia;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+}
