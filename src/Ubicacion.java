91f2065a46de45807555a3000e032591140166cc
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-25 12:07:35 -0600

    Clase Ubicacion creada

---
+++ src/Ubicacion.java
@@ -0,0 +1,15 @@
+public class Ubicacion {
+    private String nombreLugar;
+
+    public Ubicacion(String nombreLugar) {
+        this.nombreLugar = nombreLugar;
+    }
+
+    public String getNombreLugar() {
+        return nombreLugar;
+    }
+
+    public void setNombreLugar(String nombreLugar) {
+        this.nombreLugar = nombreLugar;
+    }
+}
