79cb7745021ef2bcf08b1a3417ad0592e0f3fef9
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-04-25 12:12:28 -0600

    Clase Arista creada

---
+++ src/Arista.java
@@ -0,0 +1,18 @@
+public class Arista {
+    private final String destino;
+    private final int peso;
+
+
+    public Arista(String destino, int peso) {
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+}
